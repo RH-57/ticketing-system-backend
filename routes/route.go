9ab4f4935b2fdcd5c98f71e9db7ed41fe3f88bc9
rahmat <rahmatbachtiar64@gmail.com>
2026-04-01 12:04:31 +0700

    Minnor change

--- routes/route.go
+++ routes/route.go
@@ -13,7 +13,7 @@ func SetupRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://192.168.1.185:5173"},
+		AllowOrigins:     []string{"http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
